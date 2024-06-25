38bd541952a5cffa4c7a3c2a5de982f78dded8b9
Mike <yeyuting0307@gmail.com>
2024-06-25 23:29:57 +0800

    [wip] test cnn policy

--- train/train.py
+++ train/train.py
@@ -4,12 +4,26 @@ import sys
 import glob
 sys.path.append(os.path.join(os.path.dirname(__file__), os.pardir))
 sys.path.append(os.path.join(os.pardir, 'games'))
-from dataset.make_data import episode_loader
+from mlx import core as mx
 
+from dataset.make_data import episode_loader
+from models.cnn_policy import CNNPolicy
+#%%
 episode_paths = glob.glob(os.path.join(os.pardir, "logs", "*.npz"))
 tr_iter = episode_loader(episode_paths)
+
 # %%
+cnn = CNNPolicy(3, 512)
 for batch in tr_iter:
     print(batch)
+    output = cnn(mx.array(batch["state"]))
+    print(output)
+    print(output.shape)
     break
+# %%
+
+
+
+
+
 # %%
