507ede7c04f09a876cc2edffefda421cc439be97
Mike <yeyuting0307@gmail.com>
2024-07-05 00:29:22 +0800

    more complex layer to fit

--- models/cnn_policy.py
+++ models/cnn_policy.py
@@ -10,20 +10,32 @@ class CNNPolicy(nn.Module):
             nn.Conv2d(in_channel, 32, kernel_size=7, stride=4, padding=3),
             nn.BatchNorm(32),
             nn.ReLU(),
+            nn.Dropout(0.5),
 
             nn.Conv2d(32, 64, kernel_size=5, stride=2, padding=2),
             nn.BatchNorm(64),
             nn.ReLU(),
-
-            nn.Conv2d(64, 64, kernel_size=3, stride=2, padding=1),
-            nn.BatchNorm(64),
-            nn.ReLU(),
+            nn.Dropout(0.5),
 
             nn.Conv2d(64, 128, kernel_size=3, stride=2, padding=1),
             nn.BatchNorm(128),
             nn.ReLU(),
+            nn.Dropout(0.5),
+
+            nn.Conv2d(128, 256, kernel_size=3, stride=2, padding=1),
+            nn.BatchNorm(256),
+            nn.ReLU(),
+            nn.Dropout(0.5),
+
+            nn.Conv2d(256, 512, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm(512),
+            nn.ReLU(),
+            nn.Dropout(0.5),
         )
-        self.linear = nn.Linear(38400, output_channel)
+        self.linear = nn.Sequential(
+            nn.Linear(512 * 20 * 15, output_channel),
+            nn.Dropout(0.5) 
+        )
        
     def __call__(self, x):
         output = self.cnn(x)
